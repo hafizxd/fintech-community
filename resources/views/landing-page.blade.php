a408e67c3eb6b62b7a577b7c4a81e62abfeeba73
Hafiz Aminsalim <hafizamin8116@gmail.com>
2023-01-17 06:36:27 +0700

    fix

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -8,16 +8,13 @@
                     <div class="w-full md:w-1/2 md:px-3">
                       <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
                         <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
-                          <span class="block text-green-500 xl:inline">Lorem Ipsum Dolor Sit Amet.</span>
+                          <span class="block text-green-500 xl:inline">Improve Your Finance Skills.</span>
                         </h1>
-                        <p class="mx-auto text-base text-gray-400 sm:max-w-md lg:text-xl md:max-w-3xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias quia consectetur ad quaerat doloribus aliquid?</p>
+                        <p class="mx-auto text-base text-gray-400 sm:max-w-md lg:text-xl md:max-w-3xl">A forum for exchanging information and obtaining advice related to financial issues, with access to experienced professionals for guidance.</p>
                         <div class="relative flex flex-col sm:flex-row sm:space-x-4">
                           <a href="{{ route('thread.index') }}" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-green-600 rounded-xl sm:mb-0 hover:bg-green-700 sm:w-auto">
                             Join Us
                           </a>
-                          {{-- <a href="#_" class="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
-                            Learn More
-                          </a> --}}
                         </div>
                       </div>
                     </div>
@@ -32,27 +29,27 @@
 
             {{-- Section 2 --}}
             <section class="w-full pt-7 pb-7 md:pt-20 md:pb-24 my-10 relative h-fit">
-              <h1 class="px-8 text-gray-200 font-medium text-3xl sm:text-4xl text-center max-w-md mx-auto">Lorem ipsum, dolor sit amet adipisicing elit.</h1>
+              <h1 class="px-8 text-gray-200 font-medium text-3xl sm:text-4xl text-center max-w-md mx-auto">Our Main Features</h1>
 
-              <div class="relative z-10 box-border py-10 flex flex-col items-center content-center px-8 mx-auto leading-6 text-white md:flex-row max-w-5xl lg:px-16 gap-10">
-                <x-card class="flex flex-col justify-center text-center p-8 gap-7 shadow-lg">
+              <div class="relative z-10 box-border py-10 grid grid-cols-1 md:grid-cols-2 items-center content-center px-8 mx-auto leading-6 text-white md:flex-row max-w-5xl lg:px-16 gap-10 auto-cols-fr">
+                <x-card class="flex flex-col justify-center text-center p-8 gap-7 shadow-lg h-full">
                   <div class="h-40 flex justify-center">
                     <img class="h-full w-auto" src="{{ asset('assets/images/landing-forum-2.png') }}">
                   </div>
 
                   <h1 class="text-white text-2xl sm:text-3xl font-semibold">Forum</h1>
 
-                  <p class="text-gray-400 text-sm sm:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum cupiditate aut possimus exercitationem blanditiis quia, rem odio saepe a neque.</p>
+                  <p class="text-gray-400 text-sm sm:text-base">A platform that aims to establish a comprehensive collection of questions and answers related to finance where people can find and share solutions</p>
                 </x-card>
 
-                <x-card class="flex flex-col justify-center text-center p-8 gap-7 shadow-lg">
+                <x-card class="flex flex-col justify-center text-center p-8 gap-7 shadow-lg h-full">
                   <div class="h-40 flex justify-center">
                     <img class="h-full w-auto" src="{{ asset('assets/images/landing-class.png') }}">
                   </div>
 
                   <h1 class="text-white text-2xl sm:text-3xl font-semibold">Class</h1>
 
-                  <p class="text-gray-400 text-sm sm:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum cupiditate aut possimus exercitationem blanditiis quia, rem odio saepe a neque.</p>
+                  <p class="text-gray-400 text-sm sm:text-base">A platform that offers various classes taught by experienced professionals  world to enhance and improve your financial skillsets</p>
                 </x-card>
               </div>
 
